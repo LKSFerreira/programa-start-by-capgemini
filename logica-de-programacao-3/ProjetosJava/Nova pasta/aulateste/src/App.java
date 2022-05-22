17b61240d3bd141bd601ff0fe65c03b7d43ddcc1
Lucas Ferreira <lks_ferreira@hotmail.com>
2022-05-22 09:53:10 -0300

    att

---
+++ logica-de-programacao-3/ProjetosJava/Nova pasta/aulateste/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+            int idade = leitor.nextInt();
+
+            System.out.println("Hello, World!" + idade);
+        
+    }
+}
